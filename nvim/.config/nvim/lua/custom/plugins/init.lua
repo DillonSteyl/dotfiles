80ce3cfed4c2c6179f4c1868a72a2ad68047ad27
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2025-01-21 11:26:07 +1000

    nvim: add codesnap

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -45,4 +45,19 @@ return {
       end,
     },
   },
+  -- codesnap
+  {
+    'mistricky/codesnap.nvim',
+    build = 'make',
+    keys = {
+      { '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'Save selected code snapshot into clipboard' },
+      { '<leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+    },
+    opts = {
+      save_path = '~/Pictures',
+      has_breadcrumbs = true,
+      bg_theme = 'bamboo',
+      watermark = '',
+    },
+  },
 }
