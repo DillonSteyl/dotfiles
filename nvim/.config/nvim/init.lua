5a8dbc9ec3ea1d9b43727cf7a5cb512d50ebfe54
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2025-01-24 14:19:28 +1000

    nvim: use rose-pine by default

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -976,24 +976,24 @@ require('lazy').setup({
   {
     'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+    -- init = function()
+    --   -- Load the colorscheme here.
+    --   -- Like many other themes, this one has different styles, and you could load
+    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --   vim.cmd.colorscheme 'catppuccin-mocha'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-    config = function()
-      local catppuccin = require 'catppuccin'
-      catppuccin.setup { transparent_background = true }
-    end,
+    --   -- You can configure highlights by doing something like:
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
+    -- config = function()
+    --   local catppuccin = require 'catppuccin'
+    --   catppuccin.setup { transparent_background = true }
+    -- end,
   },
 
   {
     'rose-pine/neovim',
-    priority = 1001, -- Make sure to load this before all the other start plugins.
+    priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       vim.cmd.colorscheme 'rose-pine-moon'
     end,
