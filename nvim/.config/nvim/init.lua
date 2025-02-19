5babd85e161ea7bc3aa62d75a696ad50e20b8090
Dillon Steyl <dillon.steyl@getforesight.com>
2025-02-20 09:41:23 +1000

    nvim: add ember LSP

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -751,6 +751,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         tailwindcss = {},
+        ember = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
