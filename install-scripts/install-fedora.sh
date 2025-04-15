7160cb82924e4d6fd24d70bae7c80737fc4ba755
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2025-04-15 19:16:50 +1000

    install: add fedora install script

---
+++ install-scripts/install-fedora.sh
@@ -0,0 +1,27 @@
+# CLI tools & dependencies
+sudo dnf install -y neofetch \
+	lazygit
+
+# ZSH & Prompt Setup
+sudo dnf install zsh
+sudo chsh $USER -s $(which zsh)
+# Starship setup
+curl -sS https://starship.rs/install.sh | sh
+
+# NEOVIM
+sudo dnf install -y neovim python3-neovim
+
+# LAZYGIT
+echo "  Installing lazygit..."
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit.tar.gz 
+rm lazygit
+
+# PYENV
+curl https://pyenv.run | bash
+# pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
