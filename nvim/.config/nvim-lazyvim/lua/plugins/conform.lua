0057a2e637d583a35e9228ab781bf43d11f094d8
Dillon Steyl <dillon.steyl@getforesight.com>
2025-07-15 09:24:37 +1000

    nvim-lazy: setup lsps and formatters

---
+++ nvim/.config/nvim-lazyvim/lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      gdscript = { "gdformat" },
+      -- You can use 'stop_after_first' to run the first available formatter from the list
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      vue = { "prettierd", "prettier", stop_after_first = true },
+      astro = { "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettierd", "prettier", stop_after_first = true },
+    },
+  },
+}
--- nvim/.config/nvim-lazyvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim-lazyvim/lua/plugins/lsp.lua
@@ -1,10 +1,47 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      gdscript = {},
-      gh_actions_ls = {},
-      gdshader_lsp = {},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gdscript = {},
+        gh_actions_ls = {},
+        gdshader_lsp = {},
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = "off", -- will rely on mypy for type checking. Keeps things consistent
+              },
+            },
+          },
+        },
+        ts_ls = {
+          init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = "",
+                languages = { "vue" },
+              },
+            },
+          },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "vue",
+          },
+        },
+        volar = {},
+        tailwindcss = {},
+        ember = {},
+        eslint = {},
+        csharp_ls = {},
+        astro = {},
+      },
     },
   },
 }
