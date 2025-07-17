40275b8ce490132c9969cf7a15666a86f2251584
Dillon Steyl <dillon.steyl@getforesight.com>
2025-07-18 09:12:15 +1000

    nvim: use html syntax highlighting for MJML

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,13 @@ return {
   opts = {
     auto_install = true,
   },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+    vim.filetype.add({
+      extension = {
+        mjml = "mjml",
+      },
+    })
+    vim.treesitter.language.register("html", "mjml")
+  end,
 }
