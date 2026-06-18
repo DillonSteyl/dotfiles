8ebc5b1a9035438edd0ca3d35fd3c30a4730cca9
Dillon Steyl <dillon.steyl@getforesight.com>
2026-06-18 10:44:19 +1000

    nvim: use qwen2.5 for local autocomplete with blink + minuet

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,4 +1,17 @@
 return {
   "saghen/blink.cmp",
-  opts = { keymap = { preset = "default", ["<C-l>"] = { "show", "show_documentation", "hide_documentation" } } },
+  dependencies = { "milanglacier/minuet-ai.nvim" },
+  opts = function(_, opts)
+    opts.keymap["<C-l>"] = { "show", "show_documentation", "hide_documentation" }
+    opts.keymap["<A-y>"] = require("minuet").make_blink_map()
+
+    table.insert(opts.sources.default, "minuet")
+    opts.sources.providers.minuet = {
+      name = "minuet",
+      module = "minuet.blink",
+      async = true,
+      timeout_ms = 3000,
+      score_offset = 50,
+    }
+  end,
 }
